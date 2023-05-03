17ed34bf33dc2b596a56da23368757234352a78d
jerrycool123 <jerrycool71534@gmail.com>
2023-05-04 00:52:38 +0800

    fix: typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-test -f "./packages/web/.env.production.local" || echo $WEB_PROD_EMV | base64 -d > ./packages/web/.env.production.local
-test -f "./packages/server/.env" || echo $SERVER_DEV_EMV | base64 -d > ./packages/server/.env
+test -f "./packages/web/.env.production.local" || echo $WEB_PROD_ENV | base64 -d > ./packages/web/.env.production.local
+test -f "./packages/server/.env" || echo $SERVER_PROD_ENV | base64 -d > ./packages/server/.env
 yarn
 yarn build
 yarn start
