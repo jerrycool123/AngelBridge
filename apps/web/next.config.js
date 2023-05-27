e03ce1c44896f8145f07f047675bc757c7761d03
jerrycool123 <jerrycool71534@gmail.com>
2023-05-28 05:09:45 +0800

    chore: config turbo code sharing

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ? Note: ES6 modules don't have __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@angel-bridge/common'],
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com', 'yt3.ggpht.com'],
+  },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
 };
 
