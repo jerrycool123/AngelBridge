01acb81a478f1e74fc3a502ddb43406797814dad
jerrycool123 <jerrycool71534@gmail.com>
2023-05-28 06:58:50 +0800

    fix: turbo

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-test -f "./packages/web/.env.production.local" || echo $WEB_PROD_ENV | base64 -d > ./packages/web/.env.production.local
-test -f "./packages/server/.env" || echo $SERVER_PROD_ENV | base64 -d > ./packages/server/.env
-mkdir -p ./packages/server/tessdata
-mkdir -p ./packages/server/cache
+test -f "./apps/web/.env.production.local" || echo $WEB_PROD_ENV | base64 -d > ./apps/web/.env.production.local
+test -f "./apps/server/.env" || echo $SERVER_PROD_ENV | base64 -d > ./apps/server/.env
+mkdir -p ./apps/server/tessdata
+mkdir -p ./apps/server/cache
 pnpm install
 pnpm build
 pnpm start
